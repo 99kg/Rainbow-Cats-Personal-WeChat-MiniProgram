e1ad0b9ccf8e61e25393161a1b992ed71e2e8c73
baolong.mei <baolong.mei@accenture.com>
2022-08-08 17:23:35 +0800

    20220808 daily update

---
+++ cloudfunctions/editTime/index.js
@@ -0,0 +1,23 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({ // 初始化云开发环境
+  env: cloud.DYNAMIC_CURRENT_ENV // 当前环境的常量
+})
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (context) => {
+    try {
+        // 根据待办_openid，更新time
+        return await db.collection(context.list).where({
+            _openid: context._openid
+        }).update({
+            data: {
+                date: context.value
+            }
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
--- miniprogram/pages/MainPage/index.js
+++ miniprogram/pages/MainPage/index.js
@@ -6,11 +6,26 @@ Page({
 
         userA: '',
         userB: '',
-        user: "",
+        user: '',
+
         days: 0,
+
+        textArr: ['山河浩瀚，宇宙浪漫，值得一看️', '只要有想见的人，就不是孤身一人', '妄想开始，爱情就来了', '放弃不难，但坚持一定很酷😋', '四季有春春有朝晨，朝晨有你方显珍贵🥹', '不要质疑前路，你要去的地方春暖花开🥰', '错过了落日余晖，还可以期待满天繁星🤩', '要可可爱爱，要逍遥自在🥳', '怀揣希望去努力，静待美好的出现🤗', '漫天星光沿途散播，长路尽处有灯火🤡', '爱的不是沿途风景，而是风景里有你🫰', '时间从来不语，却回答了所有问题🤟', '心存阳光，必有诗和远方', '时间是日久见人心的唯一见证者', '最好的心态是平静，最好的状态是简单❣️', '人不能白白活着，要白白胖胖地活着🤞', '总会相逢的，就像山川河流，万河归海‍️‍', '只要看到你的笑，世界就没有那么糟🐽', '平淡的日子装着最美的回忆', '我最近好像盐吃多了，咸得总是想你🍗', '愿往后余年，不负流年，不负自己', '万物皆缘，只有惜缘才能续缘', '♡ 愿你遍历山河，觉得人间值得 ♡', '愿你生活有惊喜，喜欢有回应🤗', '光怪陆离都经历，山川湖海放心里', '万物经过你，都变成柔软的样子', '🍑 ʜᴀ͟ᴘ͟ᴘ͟ʏ ᴇᴠᴇʀʏᴅᴀʏ̆̈ ', '🐈ིྀ 𝙃𝙖𝙥𝙥𝙮 𝙈𝙤𝙢𝙚𝙣𝙩 ◡̈°', '*    ˶⍤⃝˶ʜᴀᴘᴘʏ ᴅᴀʏs', '♬ ♬ ♩ ♡ ♪ ♪ ♫ ♭ ♫ ♡', '✐.ɴɪᴄᴇ ᴅᴀʏ 〰︎', '️ 今天天气是晴，心情是想你๑⃙⃘´༥`๑⃙⃘', 'ʜᴀᴠᴇ ᴀ ɴɪᴄᴇ ᴅᴀʏ︎', '𝙝𝙖𝙫𝙚 𝙖 𝙣𝙞𝙘𝙚 𝙙𝙖𝙮 𖠚ᐝ', '♡ Life+u＝sweet ♡', 'ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ ◡̈⃝', '˙Ⱉ˙ฅ 你还真是当地小有名气的可爱鬼！', '🧏‍♀️我的小皮筋持有者上线啦ෆ⃛', '𝙸 𝚕𝚘𝚟𝚎 𝚢𝚘𝚞 𝚝𝚘 𝚝𝚑𝚎 𝚖̴𝚘̴𝚘̴𝚗̴ 𝚊𝚗𝚍 𝚋𝚊𝚌𝚔 🌕', '𝙅𝙪𝙨𝙩 𝙬𝙖𝙣𝙣𝙖 𝙠͇𝙞͇𝙨͇𝙨͇ 🅨🅞🅤', '♡ 𝙂𝙞𝙫𝙚 𝙮𝙤𝙪 𝙩𝙝𝙚 𝙗𝙚𝙨𝙩 ིྀ', '𝒽𝑜𝓃𝑒𝓎 𓆡你在身边 在你身边', '˗ˏˋˎˊ˗一屋两人三餐四季💞', 'ʚ◡̈⃝ɞ 整个夏天想和你环游全世界', '—❥.°ʚ ι ℓσиɛ ʏσʋ ɞ°. ❥—', '-🌼🏻🏻ɴɪᴄᴇ ᴅᴀʏ︎',],
+        text: '',
+
+        lastTimeA: '',
+        lastTimeB: '',
     },
 
     async onShow(){
+        await wx.cloud.callFunction({name: 'getOpenId'}).then(async res => {
+            await wx.cloud.callFunction({name: 'getElementByOpenId', data: {
+                list: getApp().globalData.collectionStorageList,
+                _openid: res.result
+            }}).then(async data => {
+            })
+        })
+
         this.getCreditA()
         this.getCreditB()
         this.getUser()
@@ -19,8 +34,12 @@ Page({
             userB: getApp().globalData.userB,
         })
         this.setData({
-            days: this.getDate('2022-03-13 00:00:00', this.getNowTime())
+            days: this.getDate('2022-03-13 00:00:00', this.getNowTime()),
+            text: this.getText(this.data.textArr),
         })
+        this.getLastTimeA()
+        this.getLastTimeB()
+        this.setTime()
     },
 
     getCreditA(){
@@ -29,7 +48,7 @@ Page({
           this.setData({creditA: res.result.data[0].credit})
         })
     },
-    
+
     getCreditB(){
         wx.cloud.callFunction({name: 'getElementByOpenId', data: {list: getApp().globalData.collectionUserList, _openid: getApp().globalData._openidB}})
         .then(res => {
@@ -37,6 +56,20 @@ Page({
         })
     },
     
+    getLastTimeA(){
+        wx.cloud.callFunction({name: 'getElementByOpenId', data: {list: getApp().globalData.collectionUserList, _openid: getApp().globalData._openidA}})
+        .then(res => {
+            this.setData({lastTimeA: this.timeTransfer(res.result.data[0].date)})
+        })
+    },
+
+    getLastTimeB(){
+        wx.cloud.callFunction({name: 'getElementByOpenId', data: {list: getApp().globalData.collectionUserList, _openid: getApp().globalData._openidB}})
+        .then(res => {
+            this.setData({lastTimeB: this.timeTransfer(res.result.data[0].date)})
+        })
+    },
+
     async getUser(){
         await wx.cloud.callFunction({name: 'getOpenId'}).then(res => {
             if(res.result === getApp().globalData._openidA){
@@ -47,6 +80,16 @@ Page({
                 this.setData({
                     user: getApp().globalData.userB,
                 })
+            }
+        })
+    },
+    
+    async setTime(){
+        await wx.cloud.callFunction({name: 'getOpenId'}).then(async openid => {
+            if(getApp().globalData._openidA === openid.result){
+                wx.cloud.callFunction({name: 'editTime', data: {_openid: openid.result, value: new Date(), list: getApp().globalData.collectionUserList}})
+            } else {
+                wx.cloud.callFunction({name: 'editTime', data: {_openid: openid.result, value: new Date(), list: getApp().globalData.collectionUserList}})
             }
         })
     },
@@ -62,7 +105,7 @@ Page({
         return days;
    },
 
-   getNowTime: function() {
+    getNowTime: function() {
         var now = new Date();
         var year = now.getFullYear();
         var month = now.getMonth() + 1;
@@ -73,12 +116,35 @@ Page({
         if(day < 10) {
             day = '0' + day;
         };
-        //  如果需要时分秒，就放开
         var h = now.getHours();
         var m = now.getMinutes();
         var s = now.getSeconds();
         var formatDate = year + '-' + month + '-' + day + ' ' + h + ':' + m + ':' + s;
         return formatDate;
-  },
+    },
 
+    getText: function(textArr){
+        return textArr[Math.floor(Math.random() * (textArr.length - 1))];
+    },
+
+    timeTransfer: function(utc_datetime){
+        // 转为正常的时间格式 年-月-日 时:分:秒
+        let new_datetime = utc_datetime.split("T")[0] + " " + utc_datetime.split("T")[1].split(".")[0];
+
+        // 处理成为时间戳
+        let timestamp = new Date(new_datetime.replace(/-/g, '/')).getTime();
+        timestamp = timestamp / 1000;
+        // 增加8个小时，北京时间比utc时间多八个时区
+        timestamp = timestamp + 8 * 60 * 60;
+
+        // 时间戳转为时间
+        let date = new Date(parseInt(timestamp) * 1000);
+        let YY = date.getFullYear() + '-';
+        let MM = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+        let DD = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate());
+        let hh = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+        let mm = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+        let ss = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+        return YY + MM + DD + " " + hh + mm + ss;
+    }
 })
