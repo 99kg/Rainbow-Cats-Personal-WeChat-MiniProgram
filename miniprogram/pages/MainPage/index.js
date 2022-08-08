07bbdb5b26c66dd788c376bfc93ade45f3d104dd
baolong.mei <baolong.mei@accenture.com>
2022-08-08 12:57:38 +0800

    20220808 daily update

--- miniprogram/pages/MainPage/index.js
+++ miniprogram/pages/MainPage/index.js
@@ -6,14 +6,20 @@ Page({
 
         userA: '',
         userB: '',
+        user: "",
+        days: 0,
     },
 
     async onShow(){
         this.getCreditA()
         this.getCreditB()
+        this.getUser()
         this.setData({
             userA: getApp().globalData.userA,
             userB: getApp().globalData.userB,
+        })
+        this.setData({
+            days: this.getDate('2022-03-13 00:00:00', this.getNowTime())
         })
     },
 
@@ -30,4 +36,49 @@ Page({
             this.setData({creditB: res.result.data[0].credit})
         })
     },
+    
+    async getUser(){
+        await wx.cloud.callFunction({name: 'getOpenId'}).then(res => {
+            if(res.result === getApp().globalData._openidA){
+                this.setData({
+                    user: getApp().globalData.userA,
+                })
+            }else if(res.result === getApp().globalData._openidB){
+                this.setData({
+                    user: getApp().globalData.userB,
+                })
+            }
+        })
+    },
+
+    getDate: function(startTime,endTime) {
+        //日期格式化
+        var start_date = new Date(startTime.replace(/-/g, "/"));
+        var end_date = new Date(endTime.replace(/-/g, "/"));
+        //转成毫秒数，两个日期相减
+        var ms = end_date.getTime() - start_date.getTime();
+        //转换成天数
+        var days = parseInt(ms / (1000 * 60 * 60 * 24));
+        return days;
+   },
+
+   getNowTime: function() {
+        var now = new Date();
+        var year = now.getFullYear();
+        var month = now.getMonth() + 1;
+        var day = now.getDate();
+        if(month < 10) {
+            month = '0' + month;
+        };
+        if(day < 10) {
+            day = '0' + day;
+        };
+        //  如果需要时分秒，就放开
+        var h = now.getHours();
+        var m = now.getMinutes();
+        var s = now.getSeconds();
+        var formatDate = year + '-' + month + '-' + day + ' ' + h + ':' + m + ':' + s;
+        return formatDate;
+  },
+
 })
